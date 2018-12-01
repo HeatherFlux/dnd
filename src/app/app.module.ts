47ed96fcef5e86157a567c67299a75ae02bee4d2
Heather <hnelson@hawk.iit.edu>
2018-12-01 12:29:09 -0600

    beginning to build the scheduling system. Sawpped from npm to yarn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AdventurersComponent } from './adventurers/adventurers.component';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { ScheduleComponent } from './schedule/schedule.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
     AdventurersComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
