404c1e95c144a4e01114a34e51887e055b1dd220
Heather <hnelson@hawk.iit.edu>
2019-02-25 23:55:26 -0600

    update/level-up

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,11 +1,11 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-    { class: 'Warlock: Blade', level: 6, name: 'Daniel' },
-    { class: 'Wizard: Divination', level: 5, name: 'Ena' },
-    { class: 'Druid: Fey', level: 5, name: 'Ida' },
-    { class: 'Ranger: Beast Master', level: 6, name: 'Mel' },
-    { class: 'Warlock: Pact of the Tome', level: 5, name: 'Sizzle' },
-    { class: 'Monk: Open Palm', level: 5, name: 'Torin' },
-    { class: 'Barbarian: Ancestral Guardian', level: 6, name: 'Vlad' }
+    { class: 'Warlock: Blade', level: 7, name: 'Daniel' },
+    { class: 'Wizard: Divination', level: 7, name: 'Ena' },
+    { class: 'Druid: Fey', level: 7, name: 'Ida' },
+    { class: 'Ranger: Beast Master', level: 7, name: 'Mel' },
+    { class: 'Warlock: Pact of the Tome', level: 7, name: 'Sizzle' },
+    { class: 'Monk: Open Palm', level: 7, name: 'Torin' },
+    { class: 'Barbarian: Ancestral Guardian', level: 7, name: 'Vlad' }
 ];
