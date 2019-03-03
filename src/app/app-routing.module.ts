b7f5904bd369a07185bd7966bca7af1a71b11047
Heather <hnelson@hawk.iit.edu>
2019-03-02 18:05:10 -0600

    href

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CombatComponent } from './combat/combat.component';
 import { MainComponent } from './main/main.component';
-import { AboutComponent } from './about/about.component';
+import { AboutComponent} from './about/about.component';
 
 const routes: Routes = [
-  { path: '/', pathMatch: 'full', redirectTo: '', component: MainComponent },
+  { path: '', pathMatch: 'full', component: MainComponent },
   { path: 'combat', component: CombatComponent },
   { path: 'about', component: AboutComponent }
 ];
