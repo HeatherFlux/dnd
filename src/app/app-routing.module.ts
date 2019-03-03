c88d588bb745d39e48228117365d76f246a0f185
Heather <hnelson@hawk.iit.edu>
2019-03-02 19:23:09 -0600

    h

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ import { AboutComponent} from './about/about.component';
 
 const routes: Routes = [
   { path: 'home', component: MainComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '/', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home'},
+  { path: '/', redirectTo: 'home' },
   { path: 'combat', component: CombatComponent },
   { path: 'about', component: AboutComponent }
 ];
